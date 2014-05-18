daa3d85b1db344068a3443ff296c5b54c4d67024
Steve Kondik <shade@chemlab.org>
2014-05-18 15:34:30 -0700

    build: Include vendor/extra/BoardConfigExtra.mk if available
    Change-Id: Icd0db0852ceee5cb5c701c9a40b829e1f3ebd69e
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -440,6 +440,7 @@ endif
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
+-include vendor/extra/BoardConfigExtra.mk
 ifneq ($(CLOWN_BUILD),)
 include vendor/clown/config/BoardConfigClown.mk
 endif
